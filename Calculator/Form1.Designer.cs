1c1ee0edee3977e13c67f9baaf241b05dc01a431
vothingocchau <vothingocchau2428@gmail.com>
2023-11-05 21:02:01 +0700

    Design Form

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label2 = new Label();
+            txtA = new TextBox();
+            btPlus = new Button();
+            label1 = new Label();
+            txtB = new TextBox();
+            label3 = new Label();
+            txtResult = new TextBox();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(283, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(31, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Số A";
+            // 
+            // txtA
+            // 
+            txtA.Location = new Point(417, 86);
+            txtA.Name = "txtA";
+            txtA.Size = new Size(100, 23);
+            txtA.TabIndex = 2;
+            // 
+            // btPlus
+            // 
+            btPlus.Location = new Point(417, 280);
+            btPlus.Name = "btPlus";
+            btPlus.Size = new Size(75, 23);
+            btPlus.TabIndex = 3;
+            btPlus.Text = "Cộng";
+            btPlus.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(283, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(30, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Số B";
+            // 
+            // txtB
+            // 
+            txtB.Location = new Point(417, 143);
+            txtB.Name = "txtB";
+            txtB.Size = new Size(100, 23);
+            txtB.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(283, 207);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 15);
+            label3.TabIndex = 1;
+            label3.Text = "Kết quả";
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(417, 203);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(100, 23);
+            txtResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btPlus);
+            Controls.Add(txtResult);
+            Controls.Add(label3);
+            Controls.Add(txtB);
+            Controls.Add(label1);
+            Controls.Add(txtA);
+            Controls.Add(label2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label2;
+        private TextBox txtA;
+        private Button btPlus;
+        private Label label1;
+        private TextBox txtB;
+        private Label label3;
+        private TextBox txtResult;
     }
 }
